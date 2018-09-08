21d4e4bb8b11ccaaab94d1ad165d107443ddab79
Thiago Thomaz <thiagorthomaz@gmail.com>
2018-09-07 21:54:26 -0300

    Started to comunicate with Firestore

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # wp2firestore
 
 My goal here is to create a middleware to publish my wordpress posts into a Firestore database that I'm using on an Ionic mobile project
+
+
+
+## Dependencies
+
+sudo apt-get install php-dev php-pear phpunit
+
+sudo apt-get install g++
+sudo apt-get install libz-dev
+sudo apt-get install libz-dev
+sudo pecl install grpc
+
+Add this line to your php.ini file, e.g. /etc/php/7.2/apache2/php.ini and /etc/php/7.2/cli/php.ini
+
+extension=grpc.so
+
+composer require "grpc/grpc:^v1.1.0"
+
+composer require google/cloud-firestore
+
+composer require kreait/firebase-php ^4.16.0
+
--- app/Http/Controllers/FSPostController.php
+++ app/Http/Controllers/FSPostController.php
@@ -6,11 +6,33 @@ use Illuminate\Support\Facades\DB;
 
 use App\Http\Controllers\Controller;
 
+use Google\Cloud\Firestore\FirestoreClient;
+
+
 class FSPostController extends Controller
 {
 
     public static function show()
     {
+
+        $firestore = new FirestoreClient();
+
+        $collectionReference = $firestore->collection('noticias');
+        
+        $documents = $collectionReference->documents();
+
+
+        foreach($documents as $document) {
+
+            if ($document->exists()) {
+                print_r($document['data']);
+            }
+
+        }
+
+        //print_r($documents);
+
+        exit;
         $posts = DB::select('select * from wp_posts where post_type="post"');
         return view('posts', ['posts' => $posts]);
     }
